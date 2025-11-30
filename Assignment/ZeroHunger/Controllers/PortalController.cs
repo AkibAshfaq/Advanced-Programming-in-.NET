1906669b8e076c11d24c2a2dd76145e4eacdbfe8
AkibAshfaq <akibash.dev@gmail.com>
2025-11-30 14:30:32 +0600

    Assignment

---
+++ Assignment/ZeroHunger/Controllers/PortalController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.EF;
+
+namespace ZeroHunger.Controllers
+{
+    public class PortalController : Controller
+    {
+        ZeroHungerNGOEntities db = new ZeroHungerNGOEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AssignStatus(int id)
+        {
+            var assign = db.Requests.Find(id);
+            assign.Status = "Assigned";
+
+            return View();
+        }
+
+        public ActionResult CompleteStatus(int id)
+        {
+            var assign = db.Requests.Find(id);
+            assign.Status = "Completed";
+
+            return View();
+        }
+    }
+}
---
+++ Assignment/ZeroHunger/Controllers/ResturentController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.Models;
+using ZeroHunger.EF;
+
+namespace ZeroHunger.Controllers
+{
+    public class ResturentController : Controller
+    {
+        ZeroHungerNGOEntities db = new ZeroHungerNGOEntities();
+
+        [HttpGet]
+        public ActionResult DashBoard()
+        {
+            var requests = db.Requests.ToList();
+
+            return View(requests);
+        }
+
+        [HttpPost]
+        public ActionResult DashBoard(ResturentRequests resturent)
+        {
+            // Map ResturentRequests to Request
+            var request = new Request
+            {
+                ResturentName = resturent.ResturentName,
+                ResturentLocation = resturent.ResturentLocation,
+                PreserveDate = resturent.PreserveDate,
+                Assigned = resturent.Assigned,
+                Status = resturent.Status
+            };
+
+            db.Requests.Add(request);
+            db.SaveChanges();
+
+            var requests = db.Requests.ToList();
+
+            return View(requests);
+        }
+
+        
+    }
+}
